29e707f42ae674f4d9b314e221e9b082d50ee15e
rostyk_haidukevych <rgaidukevich9@gmail.com>
2017-11-03 15:10:23 +0200

    disapply for 2x2 format

--- unpackaged/aura/TournamentItem/TournamentItemController.js
+++ unpackaged/aura/TournamentItem/TournamentItemController.js
@@ -65,6 +65,7 @@
     applyFunc: function (component, event, helper) {
         component.set("v.refreshTournament", false);
         if (component.get("v.tournament").Format__c == '1 x 1') {
+            component.set("v.message", "Create your team for tournament");
             var action = component.get("c.applyForTournament");
             action.setParams({
                 "tournamentId": component.get("v.tournament").Id,
@@ -78,36 +79,56 @@
             })
             $A.enqueueAction(action);
         } else {
-            var action = component.get("c.playersToInviteForTeam");
-            action.setParams({
-                "tournamentId" : component.get("v.tournament").Id
-            });
-            action.setCallback(component, function (response) {
-                var state = response.getState();
-                if (state === "SUCCESS") {
-                    component.set("v.createTeamWindow", true);
-                    var data = JSON.parse(response.getReturnValue());
-                    var opts = [];
-                    var opt1 = {};
-                    opt1.label = '---None---';
-                    opt1.value = 'Null';
-                    opt1.disabled = false;
-                    opt1.selected = true;
-                    opts.push(opt1);
-                    for (var i = 0; i < data.length; i++){
-                        var opt = {};
-                        if (data[i].teamName) {
-                            opt.label = data[i].playerName + '---(' + data[i].teamName + ')';
-                        } else opt.label = data[i].playerName;
-                        opt.value = data[i].playerId;
-                        if (data[i].playerId == component.get("v.player").Id) opt.disabled = true;
-                        else opt.disabled = !data[i].enabled;
-                        opts.push(opt);
+            if (!component.get("v.isApplied")) {
+                var action = component.get("c.playersToInviteForTeam");
+                action.setParams({
+                    "tournamentId": component.get("v.tournament").Id
+                });
+                action.setCallback(component, function (response) {
+                    var state = response.getState();
+                    if (state === "SUCCESS") {
+                        component.set("v.createTeamWindow", true);
+                        var data = JSON.parse(response.getReturnValue());
+                        var opts = [];
+                        var opt1 = {};
+                        opt1.label = '---None---';
+                        opt1.value = 'Null';
+                        opt1.disabled = false;
+                        opt1.selected = true;
+                        opts.push(opt1);
+                        for (var i = 0; i < data.length; i++) {
+                            var opt = {};
+                            if (data[i].teamName) {
+                                opt.label = data[i].playerName + '---(' + data[i].teamName + ')';
+                            } else opt.label = data[i].playerName;
+                            opt.value = data[i].playerId;
+                            if (data[i].playerId == component.get("v.player").Id) opt.disabled = true;
+                            else opt.disabled = !data[i].enabled;
+                            opts.push(opt);
+                        }
+                        component.find("TeamMatesSelect").set("v.options", opts);
                     }
-                    component.find("TeamMatesSelect").set("v.options", opts);
-                }
-            })
-            $A.enqueueAction(action);
+                })
+                $A.enqueueAction(action);
+            } else {
+                var action = component.get("c.deleteTeamDialog");
+                action.setParams({
+                    "tournamentId" : component.get("v.tournament").Id,
+                    "playerCookieId" : component.get("v.player").Id
+                });
+                
+                action.setCallback(component, function (response) {
+                    var state = response.getState();
+                    if (state==="SUCCESS"){
+                        var data = response.getReturnValue();
+                        component.set("v.teamToDelete", data);
+                        component.set("v.deleteTeam", true);
+                        component.set("v.createTeamWindow", true);
+                        component.set("v.message", "Warning! You are trying to delete your team - after this operation, you will be disapplied from current tournament");
+                    }
+                })
+                $A.enqueueAction(action);
+            }
         }
     },
     
@@ -140,7 +161,28 @@
         } else {
             alert('Incorrect data, please, input the name of your team and select available teammate')
         }
+    },
+
+
+    deleteTeamFunc: function (component) {
+        var teamId = component.get("v.teamToDelete").Id;
+            var action = component.get("c.deleteTeam");
+            action.setParams({
+                "teamId" : teamId
+            });
+
+            action.setCallback(component, function (response) {
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    component.set("v.teamName", "");
+                    component.set("v.idPlayerSelected", "");
+                    component.set("v.message", "Create your team for tournament");
+                    component.set("v.deleteTeam", false);
+                    component.set("v.createTeamWindow", false);
+                    component.set("v.isApplied", !component.get("v.isApplied"));
+                    
+                }
+            })
+            $A.enqueueAction(action);
     }
-    
-    
 })
