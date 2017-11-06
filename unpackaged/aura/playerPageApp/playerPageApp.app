d48ef0a48a479171fc13cd3de9dbfecdaba696f7
rostyk_haidukevych <rgaidukevich9@gmail.com>
2017-11-06 17:05:15 +0200

    replaced player page with lightning framework, also added a possibility to upload image through the lightning

---
+++ unpackaged/aura/playerPageApp/playerPageApp.app
@@ -0,0 +1,3 @@
+<aura:application extends="force:slds" access="global">
+    <c:playerUpperInfo  idPlayerFromCookie="a000Y00000DqYt4" idCurrentPlayer="a000Y00000DqYt4"/>
+</aura:application>
---
+++ unpackaged/aura/playerUpperInfo/playerUpperInfoController.js
@@ -0,0 +1,74 @@
+({
+    /*handleUploadFinished: function (component, event, helper) {
+		var uploadedFiles = event.getSource().get("v.files");
+        alert("Files uploaded : " + uploadedFiles[0].name + ', type: '+uploadedFiles[0].type+', size: '+uploadedFiles[0].size);
+        alert(encodeURIComponent(uploadedFiles[0]));
+        
+        var action = component.get("c.uploadImage");
+        action.setParams({
+            "doc" : encodeURIComponent(uploadedFiles[0])
+            //"doc" : (uploadedFiles[0])
+        });
+        
+        action.setCallback(component, function (response) {
+                var state = response.getState();
+                alert(state);
+        })
+        $A.enqueueAction(action);
+    }*/
+    
+    
+    handleUploadFinished: function (component, event, helper) {
+        component.set("v.loadingNewImage", true);
+        var uploadedFiles = event.getSource().get("v.files");
+    	var reader = new FileReader();
+        var self = this;
+        reader.onload = function () {
+             var action = component.get("c.uploadImage");
+             action.setParams(
+                 { "doc" : encodeURIComponent(reader.result),
+                   "idPlayerFromCookie" : component.get("v.idPlayerFromCookie"), 
+                   "fileName" : uploadedFiles[0].name
+             });
+             action.setCallback(self, function(actionResult) {
+                 console.log((actionResult.getReturnValue()));
+                 var state = actionResult.getState();
+                 if (state === 'SUCCESS') {
+                     var player = component.get("v.player");
+                     player.Image__c = actionResult.getReturnValue();
+                     component.set("v.player", player);
+                     component.set("v.loadingNewImage", false);
+                 }
+             });
+             $A.enqueueAction(action);
+        };
+        reader.readAsDataURL(uploadedFiles[0]);
+    },
+    
+    initPlayer: function (component, event, helper) {
+        if (component.get("v.player") == null) {
+            var action = component.get("c.getPlayer")
+            action.setParams({
+                "idPlayer" : component.get("v.idCurrentPlayer")
+            });
+            
+            action.setCallback(component, function (response) {
+                var state = response.getState();
+                if (state === 'SUCCESS') {
+                    component.set("v.player" , response.getReturnValue());
+                    helper.initStatisticsOfPlayer(component);
+                    helper.getCurrentAndCompletedTournamentsOfPlayerForOptions(component);
+                }
+            });
+            $A.enqueueAction(action);
+        } else {
+            helper.initStatisticsOfPlayer(component);
+            helper.getCurrentAndCompletedTournamentsOfPlayerForOptions(component);
+        }
+    },
+
+    selectTournament: function (component, event, helper) {
+        helper.getStatisticsOfSelectedTournament(component);
+    }
+    
+})
